6595dc0e565f6e419cbab42ec13cf54bcaa3214a
Janith Madarasinghe <73998693+Janithpm@users.noreply.github.com>
2024-03-12 16:19:55 +0530

    constants

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        dark: '#1d252b',
+        light: '#ffffff',
+        primary: '#2eb872',
+        secondary: '#2b62f0'
       }
     }
   },
